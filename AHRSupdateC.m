6a109b0ca94dcf24625b9b01b440e152cc573720
umview <umview@163.com>
2018-10-05 21:38:12 +0800

    Q_Init

--- AHRSupdateC.m
+++ AHRSupdateC.m
@@ -30,40 +30,52 @@ function Eular = AHRSupdateC(Accel,Gyro,Mag)
     q2q3 = q2 * q3;  
     q3q3 = q3 * q3;
 
-    hx = 2.0 * (mx * (0.5 - q2q2 - q3q3) + my * (q1q2 - q0q3) + mz * (q1q3 + q0q2));
-    hy = 2.0 * (mx * (q1q2 + q0q3) + my * (0.5 - q1q1 - q3q3) + mz * (q2q3 - q0q1));
-    bx = sqrt(hx * hx + hy * hy);
-    bz = 2.0 * (mx * (q1q3 - q0q2) + my * (q2q3 + q0q1) + mz * (0.5 - q1q1 - q2q2));
-    halfvx = q1q3 - q0q2;
-    halfvy = q0q1 + q2q3;
-    halfvz = q0q0 - 0.5 + q3q3;
-    halfwx = bx * (0.5 - q2q2 - q3q3) + bz * (q1q3 - q0q2);
-    halfwy = bx * (q1q2 - q0q3) + bz * (q0q1 + q2q3);
-    halfwz = bx * (q0q2 + q1q3) + bz * (0.5 - q1q1 - q2q2);  
-    halfex = (ay * halfvz - az * halfvy) + (my * halfwz - mz * halfwy);
-	halfey = (az * halfvx - ax * halfvz) + (mz * halfwx - mx * halfwz);
-	halfez = (ax * halfvy - ay * halfvx) + (mx * halfwy - my * halfwx);
-    ErrInt(1) = ErrInt(1) + halfex * Ki * T;
-    ErrInt(2) = ErrInt(2) + halfey * Ki * T;
-    ErrInt(3) = ErrInt(3) + halfez * Ki * T;
-    gx = gx + Kp * halfex + ErrInt(1);
-    gy = gy + Kp * halfey + ErrInt(2);
-    gz = gz + Kp * halfez + ErrInt(3);
-    gx = gx * (0.5 * T);     
-    gy = gy * (0.5 * T);  
-    gz = gz * (0.5 * T);  
-    qa = q0;  
-    qb = q1;  
-    qc = q2;
-    q0 = q0 + (-qb * gx - qc * gy - q3 * gz);  
-    q1 = q1 + (qa * gx + qc * gz - q3 * gy);  
-    q2 = q2 + (qa * gy - qb * gz + q3 * gx);  
-    q3 = q3 + (qa * gz + qb * gy - qc * gx);
-    Q(1) = q0;
-    Q(2) = q1;
-    Q(3) = q2;
-    Q(4) = q3;
-    Q = Q / sqrt(sum(Q.^2));
+	halfT = T / 2;
+
+    hx = 2.0*mx*(0.5 - q2q2 - q3q3) + 2.0*my*(q1q2 - q0q3) + 2.0*mz*(q1q3 + q0q2);
+    hy = 2.0*mx*(q1q2 + q0q3) + 2.0*my*(0.5 - q1q1 - q3q3) + 2.0*mz*(q2q3 - q0q1);
+    hz = 2.0*mx*(q1q3 - q0q2) + 2.0*my*(q2q3 + q0q1) + 2.0*mz*(0.5 - q1q1 - q2q2);         
+    bx = sqrt((hx*hx) + (hy*hy));
+    bz = hz; 
+    vx = 2.0*(q1q3 - q0q2);
+    vy = 2.0*(q0q1 + q2q3);
+    vz = q0q0 - q1q1 - q2q2 + q3q3;
+    wx = 2.0*bx*(0.5 - q2q2 - q3q3) + 2.0*bz*(q1q3 - q0q2);
+    wy = 2.0*bx*(q1q2 - q0q3) + 2.0*bz*(q0q1 + q2q3);
+    wz = 2.0*bx*(q0q2 + q1q3) + 2.0*bz*(0.5 - q1q1 - q2q2);  
+    ex = (ay*vz - az*vy) + (my*wz - mz*wy);
+    ey = (az*vx - ax*vz) + (mz*wx - mx*wz);
+    ez = (ax*vy - ay*vx) + (mx*wy - my*wx);
+
+    if ex ~= 0.0 && ey ~= 0.0 && ez ~= 0.0
+    
+        ErrInt = ErrInt + ex * Ki * halfT;
+        ErrInt = ErrInt + ey * Ki * halfT;	
+        ErrInt = ErrInt + ez * Ki * halfT;
+        gx = gx + Kp*ex + ErrInt;
+        gy = gy + Kp*ey + ErrInt;
+        gz = gz + Kp*ez + ErrInt;
+    end
+    tempq0 = q0 + (-q1*gx - q2*gy - q3*gz)*halfT;
+    tempq1 = q1 + (q0*gx + q2*gz - q3*gy)*halfT;
+    tempq2 = q2 + (q0*gy - q1*gz + q3*gx)*halfT;
+    tempq3 = q3 + (q0*gz + q1*gy - q2*gx)*halfT;  
+    q0 = tempq0 ./ sqrt(tempq0 .* tempq0 + tempq1 .* tempq1 + tempq2 .* tempq2 + tempq3 .* tempq3);
+    q1 = tempq1 ./ sqrt(tempq0 .* tempq0 + tempq1 .* tempq1 + tempq2 .* tempq2 + tempq3 .* tempq3);
+    q2 = tempq2 ./ sqrt(tempq0 .* tempq0 + tempq1 .* tempq1 + tempq2 .* tempq2 + tempq3 .* tempq3);
+    q3 = tempq3 ./ sqrt(tempq0 .* tempq0 + tempq1 .* tempq1 + tempq2 .* tempq2 + tempq3 .* tempq3);
+
+    
+%     norm = sqrt(tempq0 * tempq0 + tempq1 * tempq1 + tempq2 * tempq2 + tempq3 * tempq3);
+%     q0 = tempq0 / norm;
+%     q1 = tempq1 / norm;
+%     q2 = tempq2 / norm;
+%     q3 = tempq3 / norm;
+    Q(1) = q0(1);
+    Q(2) = q1(1);
+    Q(3) = q2(1);
+    Q(4) = q3(1);
+    %Q = Q / sqrt(sum(Q.^2));
     Eular(1) = atan((2.0 * (q0q1 + q2q3)) / (q0q0 - q1q1 - q2q2 + q3q3));
     Eular(2) = -asin(2.0 * (q1q3 - q0q2));
     Eular(3) = atan((2.0 * (q1q2 + q0q3)) / (q0q0 + q1q1 - q2q2 - q3q3));
--- demo.m
+++ demo.m
@@ -2,6 +2,7 @@ clear;
 clc;
 global T;
 global Q;
+global Q0;
 global nCb;
 global bCn;
 global Kp;
@@ -15,15 +16,15 @@ global angle;
 coe = 2.0477;
 T = 0.005;
 %Q = zeros(4,1);
+Q0 = zeros(4,1);
 %nCb = zeros(3,3);
-Kp = 3;
-Ki = 0;
-ErrInt = zeros(3,1);
+Kp = 2.0;
+Ki = 0.2;
 %Eular = zeros(3,1);
 %bCn = nCb';
 gyroOffset = zeros(3,1);
-raw = load('raw_static.txt');
-getOffset(raw(:,[4, 5, 6]));
+raw = load('raw.txt');
+getOffset(raw(1:700,[4, 5, 6]));
 raw = load('raw.txt');
 N = length(raw);
 angle = zeros(3,N);
@@ -34,10 +35,59 @@ rawGyro = raw(:,[4, 5, 6])';
 rawMag = raw(:,[7, 8, 9])';
 rawGyro(:,1) = rawGyro(:,1) - gyroOffset;
 rawGyro(:,1) = rawGyro(:,1) * 1000 / 32768 * pi / 180;
-Q = [0;
-     0.05;
-     0.01;
-     0.01];
+if rawMag(1,1) < 0 && rawMag(2,1) < 0
+    if abs(rawMag(1,1) / rawMag(2,1)) >= 1
+        Q0(1) = 0.195;
+        Q0(2) = -0.015;
+        Q0(3) = 0.0043;
+        Q0(4) = 0.979;
+    else
+        Q0(1) = 0.555;
+        Q0(2) = -0.015;
+        Q0(3) = 0.006;
+        Q0(4) = 0.829;
+    end
+end
+if rawMag(1,1) < 0  && rawMag(2,1) > 0
+    if abs(rawMag(1,1) / rawMag(2,1)) >= 1
+        Q0(1) = -0.193;
+        Q0(2) = -0.009;
+        Q0(3) = -0.006;
+        Q0(4) = 0.979;
+    else
+        Q0(1) = -0.552;
+        Q0(2) = -0.0048;
+        Q0(3) = -0.0115;
+        Q0(4) = 0.8313;
+    end
+end
+if rawMag(1,1) > 0 && rawMag(2,1) > 0
+    if abs(rawMag(1,1) / rawMag(2,1)) >= 1
+        Q0(1) = -0.9785;
+        Q0(2) = 0.008;
+        Q0(3) = -0.02;
+        Q0(4) = 0.195;
+    else
+        Q0(1) = -0.9828;
+        Q0(2) = 0.002;
+        Q0(3) = -0.0167;
+        Q0(4) = 0.5557;
+    end
+end
+if rawMag(1,1) > 0 && rawMag(2,1) < 0
+    if abs(rawMag(1,1) / rawMag(2,1)) >= 1
+        Q0(1) = -0.979;
+        Q0(2) = 0.0116;
+        Q0(3) = -0.0167;
+        Q0(4) = -0.195;
+    else
+        Q0(1) = -0.83;
+        Q0(2) = 0.014;
+        Q0(3) = -0.012;
+        Q0(4) = -0.556;
+    end
+end
+Q = Q0;
 nCb = [1-2*(Q(3)^2+Q(4)^2),   2*(Q(2)*Q(3)-Q(1)*Q(4)),    2*(Q(2)*Q(4)+Q(1)*Q(3));
        2*(Q(2)*Q(3)+Q(1)*Q(4)),   1-2*(Q(2)^2+Q(4)^2),    2*(Q(3)*Q(4)-Q(1)*Q(2));
        2*(Q(2)*Q(4)-Q(1)*Q(3)),   2*(Q(3)*Q(4)+Q(1)*Q(2)),    1-2*(Q(2)^2+Q(3)^2)];
@@ -49,11 +99,9 @@ for k = 2:N
     Eular(:,k) = Eular(:,k) * 180 / pi;
 end
 figure(1);
-plot(Eular([1,2],:)');
-Q = [0;
-     0.05;
-     0.01;
-     0.01];
+plot(Eular([1,2,3],:)');
+Q = Q0;
+ErrInt = zeros(3,1);
 for k = 2:N
     rawGyro(:,k) = rawGyro(:,k) - gyroOffset;
     rawGyro(:,k) = rawGyro(:,k) * 1000 / 32768 * pi / 180;
@@ -62,7 +110,7 @@ for k = 2:N
 
 end
 figure(2)
-plot(CEular([1,2],:)');
+plot(CEular([1,2,3],:)');
 raw = raw';
 for k=1:N
     angle(2,k) = acos(raw(1,k) / sqrt(raw(1,k)^2 + raw(3,k)^2));
